15c1c3ca959e9291449b58d3fe8cc00bf17d21ac
jonareyes <jonathan_reyesv@hotmail.com>
2017-03-02 23:33:05 -0600

    This exercise is for undertand how to make variables

---
+++ definiendo_variables.rb
@@ -0,0 +1,24 @@
+#In this exercise we practice how to make basics variables.
+
+numero_entero = 6
+numero_flotante1 = 3.9
+numero_flotante2 = 4.9
+
+#In this exercise we practice how to make a boolean variables
+
+numbers = 4 > 5
+numbers = true
+
+string = "hola pepe"
+string = true
+
+numbers2 = 4 != 4
+numbers2 = false
+
+#In this exercise we practice how to make arrays with strings types.
+
+array = ["hola", "mundo", "virtual"]
+
+#In this exercise we practice how to make a hashes and how to use its
+
+hash_number = {number1: 1, number2: 5, number3: 26} 
